fa7432684e3420718efcd0dc3f1bcb41aebba512
Jorge Barrios <jorge.barrios@gmail.com>
2014-05-27 09:17:05 -0700

    WIP

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-lanterna "0.9.4"
+(defproject clojure-lanterna "2.0.0-SNAPSHOT"
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.googlecode.lanterna/lanterna "2.1.5"]]
+                 [com.googlecode.lanterna/lanterna "3.0.0-SNAPSHOT"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   )
